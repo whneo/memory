985da6cde3883ca3c22ad9326a9b097a51ea9ce1
fonzi <fonzi@5-6--9.Bildung.zz>
2016-08-22 15:13:13 +0200

    Div Container

---
+++ public_html/js/script.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var pfad = "bilder/";
+
+var image = [
+    "Bild1.jpg",
+    "Bild2.jpg",
+    "Bild3.jpg"
+];
+
+//var pfadImage = pfad + image;
+
+var i = 0;
+
+function test() {
+    for (i = 0; i < image.length; i++) {
+        var pfadImage = pfad + image[i];
+        document.getElementById("Karten" + i).src = pfadImage;
+
+    }
+}
+
+
